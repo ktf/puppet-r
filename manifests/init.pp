fc0eae7156aa2e699d73687ef52ca9be05b06d53
Trevor Bedford <t.bedford@ed.ac.uk>
2013-03-11 11:11:25 +0000

    Package type should be 'pkg'.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,13 @@
 # Examples
 #
 #   include r
+#
+# Uses http://cran.rstudio.com as source as this should redirect to a local mirror.
 class r {
 
   package { 'R_2_15_3':
-    provider => 'pkgdmg',
+    provider => 'pkg',
+    ensure => present,
     source   => 'http://cran.rstudio.com/bin/macosx/R-2.15.3.pkg',
   }
 
--- spec/classes/r_spec.rb
+++ spec/classes/r_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'r' do
   it do
     should contain_package('R_2_15_3').with({
-      :provider => 'pkgdmg',
+      :provider => 'pkg',
       :source   => 'http://cran.rstudio.com/bin/macosx/R-2.15.3.pkg',
     })
   end
