547f739460d93759b73c18e1bb5fb626eba14530
Ryan Fink <ryanjfink@gmail.com>
2015-03-21 08:35:00 -0400

    Fix broken r link

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,10 +6,17 @@
 #
 # Uses http://cran.rstudio.com as source as this should redirect to a local mirror.
 class r {
+  $version = '3.1.3'
 
-  package { 'R_3_0_2':
+  if (versioncmp($::macosx_productversion_major, '10.9') >= 0) {
+    $macv = 'mavericks'
+  } else {
+    $macv = 'snowleopard'
+  }
+
+  package { 'R':
     provider => 'pkgdmg',
-    source => 'http://cran.r-project.org/bin/macosx/R-3.0.2.pkg',
+    source => "http://cran.r-project.org/bin/macosx/R-${version}-${macv}.pkg",
     ensure => present,
   }
 
